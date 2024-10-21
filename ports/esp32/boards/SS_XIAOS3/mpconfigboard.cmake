b79736f097a31dd4fb40473b0a77b47fc0c31b26
Christopher Nadler <christopher.nadler@sick.de>
2024-10-21 10:31:00 +0000

    Update cmake file of xiao board

--- ports/esp32/boards/SS_XIAOS3/mpconfigboard.cmake
+++ ports/esp32/boards/SS_XIAOS3/mpconfigboard.cmake
@@ -2,10 +2,10 @@ set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
+    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
     boards/sdkconfig.usb
     boards/sdkconfig.ble
     boards/sdkconfig.240mhz
-    boards/sdkconfig.spiram_sx
     boards/sdkconfig.spiram_oct
     boards/SS_XIAOS3/sdkconfig.board
 )

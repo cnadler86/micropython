35a302abae6e055f6ebf8a8163c2dab9183a1736
Christopher Nadler <christopher.nadler@sick.de>
2025-03-12 09:18:19 +0000

    New boards and changes partition tables

---
+++ ports/esp32/boards/ESP32_GENERIC_S3/mpconfigvariant_FLASH_16M.cmake
@@ -0,0 +1,10 @@
+set(SDKCONFIG_DEFAULTS
+    ${SDKCONFIG_DEFAULTS}
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.spiram_oct
+    boards/ESP32_GENERIC_S3/sdkconfig.flash_4m
+)
+
+list(APPEND MICROPY_DEF_BOARD
+    MICROPY_HW_BOARD_NAME="Generic ESP32S3 module 16MB with Octal-SPIRAM"
+)
